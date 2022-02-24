f4e2f934a93eb46df8b6c6bf479df9daca20a3ee
rajansinghbaliyan <rajansinghbaliyan@gmail.com>
2022-02-24 08:13:38 +0530

    initial commit

---
+++ src/test/java/com/cherrytechnologies/restdocker/RestdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherrytechnologies.restdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
